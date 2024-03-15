c7e47662f5080672546c1559aa46a5d7a29573ee
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-03-15 09:26:43 +0000

    Creamos la clase Writer y añadimos las funciones

---
+++ writer.py
@@ -0,0 +1,12 @@
+class Writer:
+    def __init__(self, file:str)->None:
+        self.file = file
+
+    def write(self, mensaje:str)->str:
+
+        
+    def read(self)->str:
+        pass
+
+if __main__ == "__main__":
+    main()
